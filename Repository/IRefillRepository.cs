63a7dcf7ef9b7c39c4d768185ff1d2e27a7a7264
nikchoudhary <nikhilexamination@gmail.com>
2020-09-30 02:08:36 +0530

    RefillService Uploaded

---
+++ Repository/IRefillRepository.cs
@@ -0,0 +1,17 @@
+﻿using MailOrderPharmacyRefillService.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MailOrderPharmacyRefillService.Repository
+{
+    public interface IRefillRepository
+    {
+        public dynamic viewRefillStatus(int Subscription_ID);
+        //RefillDetails getRefillDuesAsOfDate(int Subscription_ID, DateTime FromDate);
+        public dynamic PendingRefill(int Sub_id, DateTime date, string Token);
+        public dynamic requestAdhocRefill(RefillOrderLine order, string Token);
+        //public IEnumerable<RefillDetails> GetAll();
+    }
+}
